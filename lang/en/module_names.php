c937a7f92f5891a0b3813cc05f3be4f70299fd64
gludovatza <attila@gludovatz.hu>
2023-08-26 12:33:57 +0200

    Localization: module names

---
+++ lang/en/module_names.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'device_types' => [
+        'label' => 'Device type',
+        'plural_label' => 'Device types',
+    ],
+    'devices' => [
+        'label' => 'Device',
+        'plural_label' => 'Devices',
+    ],
+    'documents' => [
+        'label' => 'Document',
+        'plural_label' => 'Documents',
+    ],
+];
---
+++ lang/hu/module_names.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'device_types' => [
+        'label' => 'Berendezés típus',
+        'plural_label' => 'Berendezés típusok',
+    ],
+    'devices' => [
+        'label' => 'Eszköz',
+        'plural_label' => 'Eszközök',
+    ],
+    'documents' => [
+        'label' => 'Dokumentum',
+        'plural_label' => 'Dokumentumok',
+    ],
+];
