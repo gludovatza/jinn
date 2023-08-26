f8c425e1cd7d21a7c3ebf5fa054932683313df1f
gludovatza <attila@gludovatz.hu>
2023-08-26 12:34:17 +0200

    Localization: field names

---
+++ lang/en/fields.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'nev' => 'Name',
+    'bpkod' => 'BP Code',
+    'type' => 'Type',
+    'movexkod' => 'Movex Code',
+    'uzem' => 'Plant',
+    'uzemterulet' => 'Field',
+    'tortenet' => 'History',
+    'megjegyzes' => 'Comment',
+];
---
+++ lang/hu/fields.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'nev' => 'Név',
+    'bpkod' => 'BP Kód',
+    'type' => 'Típus',
+    'movexkod' => 'Movex kód',
+    'uzem' => 'Üzem',
+    'uzemterulet' => 'Üzemterület',
+    'tortenet' => 'Történet',
+    'megjegyzes' => 'Megjegyzés',
+];
