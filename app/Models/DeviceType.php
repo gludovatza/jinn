2831f858dedc1f497480a0b5b2e809875392ce1c
gludovatza <attila@gludovatz.hu>
2023-08-26 11:28:53 +0200

    Device-Type hasmany

--- app/Models/DeviceType.php
+++ app/Models/DeviceType.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DeviceType extends Model
 {
     use HasFactory;
 
     protected $guard = ['id', 'created_at', 'updated_at'];
+
+    public function devices(): HasMany
+    {
+        return $this->hasMany(Device::class);
+    }
 }
