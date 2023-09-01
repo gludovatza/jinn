fad73403d8965c6e82414d27943fa3600d914012
gludovatza <attila@gludovatz.hu>
2023-09-01 16:42:31 +0200

    AuthServiceProvider: WorksheetPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,11 +8,13 @@ use App\Models\User;
 use App\Models\Device;
 
 use App\Models\DeviceType;
+use App\Models\Worksheet;
 use App\Policies\RolePolicy;
 use App\Policies\UserPolicy;
 use App\Policies\DevicePolicy;
 use App\Policies\DeviceTypePolicy;
 use App\Policies\PermissionPolicy;
+use App\Policies\WorksheetPolicy;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Contracts\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -32,6 +34,8 @@ class AuthServiceProvider extends ServiceProvider
         DeviceType::class => DeviceTypePolicy::class,
         Device::class => DevicePolicy::class,
         Document::class => DocumentPolicy::class,
+
+        Worksheet::class => WorksheetPolicy::class,
     ];
 
     /**
