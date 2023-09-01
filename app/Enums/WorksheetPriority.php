553378587b0334e1ca09243b799b41a2d034bd01
gludovatza <attila@gludovatz.hu>
2023-09-01 16:38:43 +0200

    worksheet: priority enum

---
+++ app/Enums/WorksheetPriority.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Contracts\HasColor;
+
+enum WorksheetPriority: string implements HasLabel, HasColor, HasIcon
+{
+    case NORMAL = 'Normál';
+    case SURGOS = 'Sürgős';
+    case LEALLASKOR = 'Leálláskor';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::NORMAL => 'Normál',
+            self::SURGOS => 'Sürgős',
+            self::LEALLASKOR => 'Leálláskor',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::NORMAL => 'gray',
+            self::SURGOS => 'warning',
+            self::LEALLASKOR => 'success',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::NORMAL => 'heroicon-m-pencil',
+            self::SURGOS => 'heroicon-m-eye',
+            self::LEALLASKOR => 'heroicon-m-check',
+        };
+    }
+}
+
+?>
