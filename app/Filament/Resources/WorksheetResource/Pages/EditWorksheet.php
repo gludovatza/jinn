b43bb4ad352a0d29d68a3a1a3186cc2eb25c19d3
gludovatza <attila@gludovatz.hu>
2023-09-01 16:40:26 +0200

    Worksheet: create+edit: creator_id and back url

---
+++ app/Filament/Resources/WorksheetResource/Pages/EditWorksheet.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\WorksheetResource\Pages;
+
+use App\Filament\Resources\WorksheetResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditWorksheet extends EditRecord
+{
+    protected static string $resource = WorksheetResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function mutateFormDataBeforeUpdate(array $data): array
+    {
+        if( ! isset($data['creator_id']))
+            $data['creator_id'] = auth()->user()->id;
+
+        return $data;
+    }
+}
