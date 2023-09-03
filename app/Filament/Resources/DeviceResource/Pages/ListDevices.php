d93c904863d36e570b33769747204a45fb9c2d67
gludovatza <attila@gludovatz.hu>
2023-09-03 21:10:40 +0200

    Device tab szűrő módosítás

--- app/Filament/Resources/DeviceResource/Pages/ListDevices.php
+++ app/Filament/Resources/DeviceResource/Pages/ListDevices.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\DeviceResource\Pages;
 
 use App\Filament\Resources\DeviceResource;
+use App\Models\Device;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\ListRecords\Tab;
@@ -22,11 +23,14 @@ class ListDevices extends ListRecords
     public function getTabs(): array
     {
         return [
-            'all' => Tab::make()->label(__('fields.all')),
             'active' => Tab::make()->label(__('fields.aktiv'))
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('aktiv', true)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('aktiv', true))
+                ->badge(Device::query()->where('aktiv', true)->count()),
+            'all' => Tab::make()->label(__('fields.all'))
+                ->badge(Device::query()->count()),
             'inactive' => Tab::make()->label(__('fields.inaktiv'))
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('aktiv', false)),
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('aktiv', false))
+                ->badge(Device::query()->where('aktiv', false)->count()),
         ];
     }
 }
